a4193a4e514115eb7aee9fae7fde3d1d9089b932
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-03-12 18:08:33 +0100

    Cleanup and optimizations of the mapper

--- src/main/java/eu/scape_project/tb/JP2ExtractionMapper.java
+++ src/main/java/eu/scape_project/tb/JP2ExtractionMapper.java
@@ -1,24 +1,5 @@
 package eu.scape_project.tb;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.log4j.Logger;
-import org.xml.sax.InputSource;
-
 import eu.scape_project.planning.model.measurement.Measure;
 import eu.scape_project.planning.model.policy.ControlPolicy;
 import eu.scape_project.planning.model.policy.ControlPolicy.ControlPolicyType;
@@ -27,136 +8,161 @@ import eu.scape_project.planning.model.policy.ControlPolicy.Qualifier;
 import eu.scape_project.tb.policy.Policy;
 import eu.scape_project.tb.policy.PolicyComparator;
 import eu.scape_project.tb.policy.PolicyReader;
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.log4j.Logger;
+import org.xml.sax.InputSource;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JP2ExtractionMapper extends Mapper<LongWritable, Text, Text, Text> {
-	private static final Logger logger = Logger.getLogger(JP2ExtractionMapper.class);
+    private static final Logger logger = Logger.getLogger(JP2ExtractionMapper.class);
 
-	@Override
-	protected void map(LongWritable key, Text inputFilePath, Context context) throws IOException, InterruptedException {
-		Text outputKey = null;
 
-		Process process = null;
-		try{
-			process = jpylize(inputFilePath, context);
-	        
-			if (process.exitValue() == 0) {
-				// transform jpylyzer output into some generic format that can be compared against a control policy
-				List<ControlPolicy> controlPolicyList = generateIndividualPolicy(process.getInputStream(), inputFilePath.toString());
+    XPath xPath = XPathFactory.newInstance().newXPath();
+    Policy organisationPolicy;
 
-				// read control policy
-				String organisationalPolicyFilePath = context.getConfiguration().get(JP2ValidationConfiguration.ORGANISATION_POLICY);
-				Policy organisationPolicy = new PolicyReader().readPolicy(organisationalPolicyFilePath);
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        // read control policy
+        String organisationalPolicyFilePath = context.getConfiguration()
+                                                     .get(JP2ValidationConfiguration.ORGANISATION_POLICY);
+        organisationPolicy = new PolicyReader().readPolicy(organisationalPolicyFilePath);
 
-				// verify control policy objectives against generic output format
-				PolicyComparator policyComparator = new PolicyComparator();
-				boolean match = policyComparator.compare(organisationPolicy, controlPolicyList);
+    }
 
-				outputKey = match ? new Text("SUCCESS") : new Text("FAILURE");
-				
-				if(!match) {
-					logger.warn(policyComparator.getLog() + " " + inputFilePath.toString());
-				}
+    @Override
+    protected void map(LongWritable key, Text inputFilePath, Context context) throws IOException, InterruptedException {
+        Text outputKey = null;
 
-			} else {
-				outputKey = new Text("FAILURE");
-				logger.error("Exit code: " + process.exitValue());
-			}
-		
-		} catch (XPathExpressionException e) {
-			outputKey = new Text("FAILURE");
-			logger.error("Exception in control policy generation, using xpath", e);
-		}
-		finally {
-			closeProcess(process);
-		}
+        Process process = null;
+        final Text failure = new Text("FAILURE");
+        final Text success = new Text("SUCCESS");
+        try {
+            process = jpylize(inputFilePath, context);
 
-		context.write(outputKey, inputFilePath);
-	}
+            if (process.exitValue() == 0) {
+                // transform jpylyzer output into some generic format that can be compared against a control policy
+                List<ControlPolicy> controlPolicyList = generateIndividualPolicy(
+                        process.getInputStream(), inputFilePath.toString());
+                // verify control policy objectives against generic output format
+                PolicyComparator policyComparator = new PolicyComparator();
+                boolean match = policyComparator.compare(organisationPolicy, controlPolicyList);
+                outputKey = match ? success : failure;
 
-	private Process jpylize(Text inputFilePath, Context context) throws IOException, InterruptedException {
-		
-		// execute command
-		List<String> commandline = Arrays.asList(
-				context.getConfiguration().get(JP2ValidationConfiguration.JPYLYZER_EXECUTABLE),
-				inputFilePath.toString());
+                if (!match) {
+                    logger.warn(policyComparator.getLog() + " " + inputFilePath.toString());
+                }
 
-		ProcessBuilder pb = new ProcessBuilder(commandline);
-		Process process = pb.start();
-		process.waitFor();
-		return process;
-	}
+            } else {
+                outputKey = failure;
+                logger.error("Exit code: " + process.exitValue());
+            }
 
-	private void closeProcess(Process process) throws IOException {
-		if (process != null) {
-			if (process.getInputStream() != null)
-				process.getInputStream().close();
-			if (process.getErrorStream() != null)
-				process.getErrorStream().close();
-			if (process.getOutputStream() != null)
-				process.getOutputStream().close();
-			process = null;
-		}
-	}
+        } catch (XPathExpressionException e) {
+            outputKey = failure;
+            logger.error("Exception in control policy generation, using xpath", e);
+        } finally {
+            closeProcess(process);
+        }
 
-	/**
-	 * this will be replaced with some converter/transformer tool-specific output -> generic output
-	 * @param jpylyzerMetadata
-	 * @return
-	 * @throws IOException
-	 * @throws XPathExpressionException 
-	 */
-	private List<ControlPolicy> generateIndividualPolicy(InputStream jpylyzerMetadata, String inputPath) throws IOException, XPathExpressionException {
-		List<ControlPolicy> controlPolicyList = new ArrayList<ControlPolicy>();
-		
-		StringWriter writer = new StringWriter();
-		IOUtils.copy(jpylyzerMetadata, writer, "UTF-8");
-		String jpylyzerMetadataAsString = writer.toString();
+        context.write(outputKey, inputFilePath);
+    }
 
-		String expression = "/jpylyzer/properties/jp2HeaderBox/colourSpecificationBox/meth";
-		try {
-			XPath xPath = XPathFactory.newInstance().newXPath();
+    private Process jpylize(Text inputFilePath, Context context) throws IOException, InterruptedException {
 
-			String value = xPath.evaluate(expression, new InputSource(new StringReader(jpylyzerMetadataAsString)));
-			Measure measure1 = new Measure();
-			measure1.setUri("http://purl.org/DP/quality/measures#18");
-			ControlPolicy cp1 = new ControlPolicy();
-			cp1.setMeasure(measure1);
-			cp1.setControlPolicyType(ControlPolicyType.FORMAT_OBJECTIVE);
-			cp1.setModality(Modality.MUST);
-			cp1.setQualifier(Qualifier.EQ);
-			cp1.setValue(value);
+        // execute command
+        List<String> commandline = Arrays.asList(
+                context.getConfiguration().get(JP2ValidationConfiguration.JPYLYZER_EXECUTABLE),
+                inputFilePath.toString());
 
-			expression = "/jpylyzer/properties/jp2HeaderBox/colourSpecificationBox/enumCS";
-			value = xPath.evaluate(expression, new InputSource(new StringReader(jpylyzerMetadataAsString)));
-			Measure measure2 = new Measure();
-			measure2.setUri("http://purl.org/DP/quality/measures#19");
-			ControlPolicy cp2 = new ControlPolicy();
-			cp2.setMeasure(measure2);
-			cp2.setControlPolicyType(ControlPolicyType.FORMAT_OBJECTIVE);
-			cp2.setModality(Modality.MUST);
-			cp2.setQualifier(Qualifier.EQ);
-			cp2.setValue(value);
+        ProcessBuilder pb = new ProcessBuilder(commandline);
+        Process process = pb.start();
+        process.waitFor();
+        return process;
+    }
 
-			expression = "/jpylyzer/properties/jp2HeaderBox/imageHeaderBox/bPCDepth";
-			value = xPath.evaluate(expression, new InputSource(new StringReader(jpylyzerMetadataAsString)));
-			Measure measure3 = new Measure();
-			measure3.setUri("http://purl.org/DP/quality/measures#20");
-			ControlPolicy cp3 = new ControlPolicy();
-			cp3.setMeasure(measure3);
-			cp3.setControlPolicyType(ControlPolicyType.FORMAT_OBJECTIVE);
-			cp3.setModality(Modality.MUST);
-			cp3.setQualifier(Qualifier.EQ);
-			cp3.setValue(value);
+    private void closeProcess(Process process) throws IOException {
+        if (process != null) {
+            if (process.getInputStream() != null) {
+                process.getInputStream().close();
+            }
+            if (process.getErrorStream() != null) {
+                process.getErrorStream().close();
+            }
+            if (process.getOutputStream() != null) {
+                process.getOutputStream().close();
+            }
+            process = null;
+        }
+    }
 
-			controlPolicyList.add(cp1);
-			controlPolicyList.add(cp2);
-			controlPolicyList.add(cp3);
-		} catch (XPathExpressionException e) {
-			String loginfo = inputPath + "; " + expression + "; " + jpylyzerMetadataAsString + ";";
-			logger.error(loginfo);
-			throw e;
-		}
+    /**
+     * this will be replaced with some converter/transformer tool-specific output -> generic output
+     *
+     * @param jpylyzerMetadata
+     *
+     * @return
+     * @throws IOException
+     * @throws XPathExpressionException
+     */
+    private List<ControlPolicy> generateIndividualPolicy(InputStream jpylyzerMetadata, String inputPath) throws
+                                                                                                         IOException,
+                                                                                                         XPathExpressionException {
+        List<ControlPolicy> controlPolicyList = new ArrayList<ControlPolicy>();
 
-		return controlPolicyList;
-	}	
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(jpylyzerMetadata, writer, "UTF-8");
+        String jpylyzerMetadataAsString = writer.toString();
+        try {
+            ControlPolicy cp1 = makeControlPolicy(
+                    "/jpylyzer/properties/jp2HeaderBox/colourSpecificationBox/meth",
+                    jpylyzerMetadataAsString,
+                    "http://purl.org/DP/quality/measures#18");
+            ControlPolicy cp2 = makeControlPolicy(
+                    "/jpylyzer/properties/jp2HeaderBox/colourSpecificationBox/enumCS",
+                    jpylyzerMetadataAsString,
+                    "http://purl.org/DP/quality/measures#19");
+            ControlPolicy cp3 = makeControlPolicy(
+                    "/jpylyzer/properties/jp2HeaderBox/imageHeaderBox/bPCDepth",
+                    jpylyzerMetadataAsString,
+                    "http://purl.org/DP/quality/measures#20");
+
+            controlPolicyList.add(cp1);
+            controlPolicyList.add(cp2);
+            controlPolicyList.add(cp3);
+        } catch (XPathExpressionException e) {
+            String loginfo = inputPath  + "; " + jpylyzerMetadataAsString + ";";
+            logger.error(loginfo,e);
+            throw new RuntimeException(e);
+        }
+
+        return controlPolicyList;
+    }
+
+    private ControlPolicy makeControlPolicy(String expression, String jpylyzerMetadataAsString, String uri) throws
+                                                                                                            XPathExpressionException {
+        String value = xPath.evaluate(expression, new InputSource(new StringReader(jpylyzerMetadataAsString)));
+        Measure measure3 = new Measure();
+        measure3.setUri(uri);
+        ControlPolicy cp3 = new ControlPolicy();
+        cp3.setMeasure(measure3);
+        cp3.setControlPolicyType(ControlPolicyType.FORMAT_OBJECTIVE);
+        cp3.setModality(Modality.MUST);
+        cp3.setQualifier(Qualifier.EQ);
+        cp3.setValue(value);
+        return cp3;
+    }
 }
